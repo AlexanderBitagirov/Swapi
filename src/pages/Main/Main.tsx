dc719b073ee526c60bc01b87086c98cb0b0cb7ee
DESKTOP-MJQAUR8\alexa <bitagirov@gmail.com>
2022-10-01 19:22:18 +0300

    added content to main page and 404

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,23 @@
-import { withLayout } from "../../layout/Layout"
+import Button from "../../components/Button/Button";
+import { withLayout } from "../../layout/Layout";
+import styles from "./Main.module.css";
+import Banner from "./banner.png";
 
 const MainPage = () => {
-
-    return(
-        <div>
-            123
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Find all your favorite character</h1>
+        <h2 className={styles.info}>
+          You can find out all the information about your favorite characters
+        </h2>
+        <Button to="/characters" appearance="orange">
+          See more...
+        </Button>
+      </div>
+      <img className={styles.banner} src={Banner} alt="banner" />
+    </div>
+  );
+};
 
 export default withLayout(MainPage);
