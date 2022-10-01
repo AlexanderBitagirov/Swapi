38f4280f12c63805a6208c54a58b36ce841e7cf6
DESKTOP-MJQAUR8\alexa <bitagirov@gmail.com>
2022-10-01 17:32:35 +0300

    a bit fix

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,11 @@ module.exports = {
       {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-      }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   resolve: {
